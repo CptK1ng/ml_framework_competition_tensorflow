327b94911b8d65e3922cbd6b15b296bb305d80eb
Lukas Theurer <thlu1011@hs-karlsruhe.de>
2018-12-16 10:06:17 +0100

    Still not working

--- scripts/OneLayerNN.py
+++ scripts/OneLayerNN.py
@@ -17,23 +17,26 @@ class OneLayerNeuralNet():
 
 
     def one_layer_network_model(self,data):
-        fully_connected_layer = {'weights': tf.Variable(tf.random_normal([9216, self.hl_size])),
+        fully_connected_layer = {'weights': tf.Variable(tf.random_normal([9216, self.hl_size],stddev=10)),
                                  'biases': tf.Variable(tf.random_normal([self.hl_size]))}
 
         l1 = tf.add(tf.matmul(data, fully_connected_layer['weights']), fully_connected_layer['biases'])
         l1 = tf.nn.relu(l1)
 
         # Output are 30 Keypount,(15 x and y coordinates for the facial keypoints
-        output_layer = {'weights': tf.Variable(tf.random_normal([self.hl_size, 30])),
+        output_layer = {'weights': tf.Variable(tf.random_normal([self.hl_size, 30],stddev=10)),
                         'biases': tf.Variable(tf.random_normal([30])), }
 
         output = tf.matmul(l1, output_layer['weights']) + output_layer['biases']
+
         return output
 
     def train(self,x, epochs, batch_size):
         prediction = self.one_layer_network_model(self.x)
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=self.y ))
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+        #cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=self.y ))
+        cost = tf.reduce_mean(tf.losses.mean_squared_error(labels=self.y, predictions=prediction))
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001).minimize(cost)
+        #optimizer = tf.train.AdamOptimizer().minimize(cost)
 
         hm_epochs = epochs
         with tf.Session() as sess:
@@ -54,12 +57,13 @@ class OneLayerNeuralNet():
                     epoch_loss += c
 
                 print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+                '''
             #TODO correct muss angepasst werden. Argmax ist glaub ich nicht nötig
             correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
 
             accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
             print('Accuracy:', accuracy.eval({x: self.data_loader.images, y: self.data_loader.keypoints}))
-
+            '''
 
 
 OneLayerNeuralNet(path_to_data="../data/training.csv", hl_size=500)
