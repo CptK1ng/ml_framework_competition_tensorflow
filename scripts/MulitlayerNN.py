80c1831749993a66f60f6b2f3a1d31e35ab7c51f
Lukas Theurer <thlu1011@hs-karlsruhe.de>
2019-01-05 16:00:25 +0100

    Fixed Bug with variable input shape

--- scripts/MulitlayerNN.py
+++ scripts/MulitlayerNN.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from setuptools.command.test import test
+from sklearn.model_selection import train_test_split
 
 from tensorflow.python import debug as tf_debug
 from scripts.DataLoader import DataLoader
@@ -20,7 +22,7 @@ class MultiLayerNeuralNet():
         with tf.name_scope(name):
             weights = tf.Variable(tf.truncated_normal([3, 3, size_in, size_out], stddev=0.1), name="weights")
             biases = tf.Variable(tf.constant(0.1, shape=[size_out]), name="biases")
-            conv = tf.nn.conv2d(input, weights, strides=[1, 1, 1, 1], padding="SAME")
+            conv = tf.nn.conv2d(input, weights, strides=[1, 1, 1, 1], padding="VALID")
             act = tf.nn.relu(conv + biases)
             tf.summary.histogram("weights", weights)
             tf.summary.histogram("biases", biases)
@@ -46,11 +48,11 @@ class MultiLayerNeuralNet():
         conv5 = self.conv_layer(conv4, 128, 256, "conv5")
 
         # Flatten the array to make it processable for fc layers
-        flattened = tf.layers.Flatten()(conv5) #
-        fcl1 = self.fc_layer(flattened, 2304, 500, "fcl1")
-        fcl2 = self.fc_layer(fcl1, 500, 500, "fcl1")
-        fcl3 = self.fc_layer(fcl2, 500, 500, "fcl1")
-        fcl4 = self.fc_layer(fcl3, 500, 500, "fcl1")
+        flattened = tf.layers.Flatten()(conv5)  #
+        fcl1 = self.fc_layer(flattened, int(flattened.shape[1]), 500, "fcl1")
+        fcl2 = self.fc_layer(fcl1, 500, 500, "fcl2")
+        fcl3 = self.fc_layer(fcl2, 500, 500, "fcl3")
+        fcl4 = self.fc_layer(fcl3, 500, 500, "fcl4")
         output = self.fc_layer(fcl4, 500, 30, "output")
 
         return output
@@ -59,29 +61,34 @@ class MultiLayerNeuralNet():
         prediction = self.le_net_model(self.x_image)
 
         with tf.name_scope("loss"):
-            loss = tf.reduce_mean(tf.losses.mean_squared_error(labels=self.y, predictions=prediction))
+            loss = tf.reduce_mean(tf.losses.mean_squared_error(labels=self.y, predictions=test_pred))
             tf.summary.scalar("mse", loss)
 
         with tf.name_scope("train"):
             optimizer = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
 
+        with tf.name_scope("test"):
+            testopt = tf.train.AdamOptimizer(learning_rate=0.01).minimize(test_loss)
+
         summ = tf.summary.merge_all()
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             writer = tf.summary.FileWriter(
-                '../tmp/facial_keypoint/le_net/{}epochs_{}bs_Adam_lr{}_{}'.format(epochs, batch_size, learning_rate, datetime.datetime.now().time()))
+                '../tmp/facial_keypoint/le_net/{}epochs_{}bs_Adam_lr{}_{}'.format(epochs, batch_size, learning_rate,
+                                                                                  datetime.datetime.now().time()))
             writer.add_graph(sess.graph)
 
             # Training procedure
             # Making one dimensional array from 2 dim image
             x_data = np.array([np.ravel(x) for x in self.data_loader.images])
             y_data = self.data_loader.keypoints
+            x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2)
             for epoch in range(epochs):
                 epoch_loss = 0
 
-                total_batches = int(len(self.data_loader.images) / batch_size)
-                x = np.array_split(x_data, total_batches)
-                y = np.array_split(y_data, total_batches)
+                total_batches = int(len(x_train) / batch_size)
+                x = np.array_split(x_train, total_batches)
+                y = np.array_split(y_train, total_batches)
 
                 for i in range(total_batches):
                     batch_x, batch_y = np.array(x[i]), np.array(y[i])
@@ -93,8 +100,10 @@ class MultiLayerNeuralNet():
                     s = sess.run(summ, feed_dict={self.x: batch_x, self.y: batch_y})
                     writer.add_summary(s, epoch)
 
+
+
                 print('Epoch', epoch, 'completed out of', epochs, 'loss:', epoch_loss)
 
 
 ml_network = MultiLayerNeuralNet(path_to_data="../data/training.csv")
-ml_network.train(5e-2, 200, 16)
+ml_network.train(1e-2, 750, 50)
